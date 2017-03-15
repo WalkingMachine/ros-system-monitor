8fe74e278aa93769d6d25b10a4a3ca2ffad4062f
Lucas Maurice <lucas.maurice@outlook.com>
2017-03-15 18:38:12 -0400

    Update for compatibilities

--- bin/cpu_monitor.py
+++ bin/cpu_monitor.py
@@ -108,7 +108,7 @@ class CPUMonitor():
 
         self._temps_timer = None
         self._usage_timer = None
-        
+
         # Get temp_input files
         self._temp_vals = self.get_core_temp_names()
 
@@ -141,18 +141,18 @@ class CPUMonitor():
         self.check_temps()
         self.check_usage()
 
-    # Restart temperature checking 
+    # Restart temperature checking
     def _restart_temp_check(self):
         rospy.logerr('Restarting temperature check thread in cpu_monitor. This should not happen')
         try:
             with self._mutex:
                 if self._temps_timer:
                     self._temps_timer.cancel()
-                
+
             self.check_temps()
         except Exception, e:
             rospy.logerr('Unable to restart temp thread. Error: %s' % traceback.format_exc())
-            
+
 
     ## Must have the lock to cancel everything
     def cancel_timers(self):
@@ -428,11 +428,11 @@ class CPUMonitor():
 
         with self._mutex:
             self._last_temp_time = rospy.get_time()
-            
+
             self._temp_stat.level = diag_level
             self._temp_stat.message = message
             self._temp_stat.values = diag_vals
-            
+
             if not rospy.is_shutdown():
                 self._temps_timer = threading.Timer(5.0, self.check_temps)
                 self._temps_timer.start()
@@ -443,7 +443,7 @@ class CPUMonitor():
         if rospy.is_shutdown():
             with self._mutex:
                 self.cancel_timers()
-            return 
+            return
 
         diag_level = 0
         diag_vals = [ KeyValue(key = 'Update Status', value = 'OK' ),
@@ -462,14 +462,14 @@ class CPUMonitor():
         if mp_level > 0:
             diag_msgs.append(mp_msg)
         diag_level = max(diag_level, mp_level)
-            
+
         # Check uptime
         uptime_level, up_msg, up_vals = self.check_uptime()
         diag_vals.extend(up_vals)
         if uptime_level > 0:
             diag_msgs.append(up_msg)
         diag_level = max(diag_level, uptime_level)
-        
+
         if diag_msgs and diag_level > 0:
             usage_msg = ', '.join(set(diag_msgs))
         else:
@@ -480,9 +480,9 @@ class CPUMonitor():
             self._last_usage_time = rospy.get_time()
             self._usage_stat.level = diag_level
             self._usage_stat.values = diag_vals
-            
+
             self._usage_stat.message = usage_msg
-            
+
             if not rospy.is_shutdown():
                 self._usage_timer = threading.Timer(5.0, self.check_usage)
                 self._usage_timer.start()
@@ -504,10 +504,10 @@ class CPUMonitor():
                 self._diag_pub.publish(msg)
                 self._last_publish_time = rospy.get_time()
 
-        
+
         # Restart temperature checking if it goes stale, #4171
         # Need to run this without mutex
-        if rospy.get_time() - self._last_temp_time > 90: 
+        if rospy.get_time() - self._last_temp_time > 90:
             self._restart_temp_check()
 
 
